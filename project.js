58be3192998d3cf4f2cea5e26b4fd86620cd6685
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-05-30 21:17:09 +0530

    spin

--- project.js
+++ project.js
@@ -1,5 +1,44 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A : 2,
+    B : 4,
+    C : 6,
+    D : 8
+};
+
+const SYMBOLS_VALUE = {
+    A : 5,
+    B : 4,
+    C : 3,
+    D : 2
+};
+
+const spin = ()=>{
+    const symbols = [];
+    for(const[symbol , count] of Object.entries(SYMBOLS_COUNT)){
+       for(i=0 ; i<count ; i++){
+        symbols.push(symbol);
+       }
+    }
+    const reels = [];
+for(let i=0 ; i<COLS ; i++){
+    reels.push([]);
+    const reelSymbols = [...symbols]
+    for(let j=0 ; j<ROWS ; j++){
+        const randomIndex = Math.floor( Math.random() * reelSymbols.length);
+        const selectedSymbol = reelSymbols[randomIndex];
+        reels[i].push(selectedSymbol);
+        reelSymbols.splice(randomIndex,1);
+    }
+}
+return reels;
+};
+
+
 const deposit = ()=>{
    while(true){
     let depositAmount = prompt("Enter a deposit Amount : ");
@@ -30,13 +69,13 @@ const getNumberOfLines = ()=>{
 }
 
 
-const getBetAmount = ()=>{
+const getBetAmount = (balance , lines)=>{
     while(true){
-        let BetAmount = prompt("Enter the bet Amount : ");
+        let BetAmount = prompt("Enter the bet amount per line : ");
         let numericBetAmount = parseFloat(BetAmount);
     
-        if(isNaN(numericBetAmount) || numericBetAmount<=0 || numericBetAmount>depositAmount){
-            console.log("Invalid deposit amount!")
+        if(isNaN(numericBetAmount) || numericBetAmount<=0 || numericBetAmount>(balance/lines)){
+            console.log("Invalid Bet amount!")
         }
         else{
             return numericBetAmount;
@@ -44,13 +83,13 @@ const getBetAmount = ()=>{
     }
 }
 
-const getNewBalance = ()=>{
-    
-}
 
-let depositAmount = deposit();
-// console.log(depositAmount);
+let balance = deposit();
+// console.log(balance);
 let noOfLines = getNumberOfLines();
 // console.log(noOfLines);
-let BetAmount = getBetAmount();
+let BetAmount = getBetAmount(balance, noOfLines);
 //  console.log(BetAmount);
+const  reels = spin();
+// console.log(reels);
+
